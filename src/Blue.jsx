9333f6c30328aa8e3fd5d655e7aaa7a58ee0b0a5
Char B <charburton18@gmail.com>
2024-04-02 09:36:46 -0700

    functional react color picker app

---
+++ src/Blue.jsx
@@ -0,0 +1,5 @@
+const Blue = () => {
+  return <div className='blue'><h1>Blue</h1></div>
+}
+
+export default Blue
---
+++ src/Red.jsx
@@ -0,0 +1,5 @@
+const Red = () => {
+  return <div className='red'><h1>Red</h1></div>
+}
+
+export default Red
